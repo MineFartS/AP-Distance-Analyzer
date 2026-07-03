4c999c1f7ae9df32c1036b337c79dad37cf58056
Phil Hunt <philiphuntiv2@gmail.com>
2026-07-03 15:23:23 -0400

    Track Distance

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
-from winrt.windows.devices.geolocation import Geolocator
+from winrt.windows.devices.geolocation import Geolocator, PositionAccuracy
+from geopy.distance import geodesic
+from datetime import timedelta
+from time import sleep
 
-gl = Geolocator()
+#======================================================
 
-pos = gl.get_geoposition_async().get()
+records: list[dict] = []
 
-print(f"Latitude: {pos.coordinate.latitude:f}")
-print(f"Longitude: {pos.coordinate.longitude:f}")
 
+while True:
+
+    gl = Geolocator()
+    gl.desired_accuracy = PositionAccuracy.HIGH
+
+    pos = gl.get_geoposition_async_with_age_and_timeout(
+        timedelta(seconds=0), # Max Age
+        timedelta(seconds=15) # Timeout
+    ).get().coordinate
+
+    records += [{
+        'coords': [
+            pos.latitude,
+            pos.longitude
+        ],
+        'ping': -1
+    }]
+    
+    dist = geodesic(
+        records[0]['coords'],
+        records[-1]['coords']
+    ).meters
+
+    print()
+    print(f'{records[0]['coords']=}')
+    print(f'{records[-1]['coords']=}')
+    print(f"Distance: {dist:f} m")
+
+    sleep(1)
